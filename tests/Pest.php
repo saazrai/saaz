8022852ce8e99a45df25c33f2fe0a8e9ccee74ad
saazrai <saaz.rai@gmail.com>
2025-07-17 02:34:01 +0530

    Move unit tests to tests/Unit, update Pest config, and clean up test suite

--- tests/Pest.php
+++ tests/Pest.php
@@ -19,6 +19,10 @@ pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
+pest()->extend(Tests\TestCase::class)
+ // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Unit');
+
 /*
 |--------------------------------------------------------------------------
 | Expectations
